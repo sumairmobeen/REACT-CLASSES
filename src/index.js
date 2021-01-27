b26408c2a5723773a478efd41e0d2890b2aef5bd
SaharSuleman <qazirameezahmed569@gmail.com>
2021-01-27 17:02:34 +0500

    'class-1'

--- src/index.js
+++ src/index.js
@@ -4,14 +4,40 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+function Hi() {
+    return <div>
+        <div className="myClass">My Name Is Mobeen</div>
+        <h1> Country Name</h1>
+        <div className="myList">
+            <ul>
+                <li>Pkaistan</li>
+                <li>Turkey</li>
+                <li>England</li>
+                <li>Japan</li>
+                <li>Canada</li>
+            </ul>
+        </div>
+        <div className="add">
+            {2+3}
+            
+        </div>
+
+ </div>
+}
+
+
+ReactDOM.render(<Hi /> , document.querySelector('#root'));
+
